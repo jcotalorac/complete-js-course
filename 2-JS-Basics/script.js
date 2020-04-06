5ecf50cad7a980823d6aee051c4c8bc10d44cf75
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-06 17:40:32 -0500

    Logical operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -53,13 +53,19 @@ console.log(lastName);*/
 
 var year, yearJohn, yearMark;
 now = 2018;
+ageJohn = 28;
+ageMark = 33;
 
 // Math operators
-yearJohn = now - 28;
-yearMark = now - 33;
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
 
 console.log(yearJohn);
 
 console.log(now + 2);
 console.log(now * 2);
 console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
