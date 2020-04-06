1fadba35a24090efe11f77dcdd5204015d108634
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-06 17:47:57 -0500

    Use of typeof operator

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -69,3 +69,10 @@ console.log(now / 10);
 // Logical operators
 var johnOlder = ageJohn > ageMark;
 console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
