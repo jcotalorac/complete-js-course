060fd166f719c0041cbcf632f5dcd118c42aebcc
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-06 22:29:10 -0500

    Else block

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -121,4 +121,6 @@ var civilStatus = 'married';
 
 if (civilStatus === 'married') {
     console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
 }
