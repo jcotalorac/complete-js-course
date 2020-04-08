8e2c6bc67c4d9fba0627f3a3e3e10e9d9ce67704
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-08 11:18:24 -0500

    Function expression and execution calling

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -6,3 +6,9 @@ calculateAge(1965);
 function calculateAge(year) {
     console.log(2016 - year);
 }
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
+
+retirement(1990);
