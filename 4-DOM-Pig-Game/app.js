d7ad6501759feb4cb8d18216c5564721ce20d72c
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-08 15:51:52 -0500

    Setting property value for style by dom manipulation

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -20,3 +20,5 @@ document.querySelector('#current-' + activePlayer).textContent = dice;
 
 var x = document.querySelector('#score-0').textContent;
 console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
