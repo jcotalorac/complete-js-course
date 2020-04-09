80018200fdea14f10a0f98ccbac8560c52f80d9e
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-09 00:19:05 -0500

    Setting player scores

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -51,8 +51,9 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] += roundScore;
     document.getElementById('score-' + activePlayer).textContent =
-    document.getElementById('current-' + activePlayer).textContent;
+    scores[activePlayer];
     document.getElementById('current-' + activePlayer).textContent = '0';
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
