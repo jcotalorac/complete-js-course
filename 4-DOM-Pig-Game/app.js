76f0100df047a7e5f71c3119175744e2ad67fdb8
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-08 22:22:17 -0500

    Restricting scope of dice variable

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -7,13 +7,13 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score-0').textContent;
@@ -27,5 +27,5 @@ function btn() {
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
-    dice = Math.floor(Math.random() * 6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 });
