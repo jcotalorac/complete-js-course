184e37f78fe5af2d66fdc93f5ed5ed61d4384144
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-09 00:56:50 -0500

    Removing next player class

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -56,6 +56,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     } else {
         nextPlayer();    
     }
