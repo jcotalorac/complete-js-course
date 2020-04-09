9f775d1b3a56241a78c93bd5e08e956997bdab43
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-09 00:53:46 -0500

    Adding winner class

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -55,6 +55,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     if(scores[activePlayer] >= 20) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     } else {
         nextPlayer();    
     }
