e7e86c5fe9a74ac72ca35b29284a3c2f5aa5d7e1
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-09 23:12:53 -0500

    Removing winner class from init

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -70,6 +70,9 @@ function init() {
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
 
