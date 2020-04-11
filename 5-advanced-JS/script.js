941d7ec4c1212b422917ce399141ca4c2e5e4402
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-10 22:10:09 -0500

    Creating object passing prototype and object as arguments

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -43,3 +43,9 @@ var john = Object.create(personProto);
 john.name = 'John';
 john.yearOfBirth = 1990;
 john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
