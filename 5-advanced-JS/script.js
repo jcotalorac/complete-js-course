1d2eb9c780b3cf8974697575e1479d19b7b09a83
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-10 20:58:39 -0500

    Inheritance through prototype property of constructor

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -10,9 +10,10 @@ var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function() {
-        console.log(2016 - this.yearOfBirth);
-    }
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
 }
 
 var john = new Person('John', 1990, 'teacher');
