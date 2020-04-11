c14413c86f32ca87ac0f0a2132ce013e6135b988
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-10 23:12:35 -0500

    Function that returns a function

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -84,6 +84,7 @@ console.log(age, obj.city);*/
 ////////////////////////////
 // Functions as arguments
 
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -105,3 +106,23 @@ function isFullAge(el) {
 var ages = arrayCalc(years, calculateAge);
 var fullAges = arrayCalc(ages, isFullAge)
 console.log(ages, fullAges);
+*/
+
+///////////////////////////
+// Functions that returns functions
+
+function interviewQuestion(job) {
+    if(job === 'designer') {
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
