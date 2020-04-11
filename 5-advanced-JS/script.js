5ab0380c6af0807bca2a54be5c5f0f646e3d22d0
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-11 12:14:02 -0500

    Invoking function expression with arguments

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -151,3 +151,8 @@ game();
    var score = Math.random() * 10;
     console.log(score >= 5); 
 })();
+
+(function(goodLuck) {
+   var score = Math.random() * 10;
+    console.log(score >= 5); 
+})(5);
