fb68c55f930deee1e8d3fab05b9fb61108439309
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-10 20:48:02 -0500

    Inheritance through function constructor

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -17,3 +17,10 @@ var Person = function(name, yearOfBirth, job) {
 
 var john = new Person('John', 1990, 'teacher');
 john.calculateAge();
+
+var jane = new Person('Jane', 1969, 'designer');
+
+var mark = new Person('Mark', 1948, 'retired');
+
+jane.calculateAge();
+mark.calculateAge();
