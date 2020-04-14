29cc32ceea34794ea343c41b18034bc34d6283b6
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-14 17:20:58 -0500

    Parameterizing DOM strings names

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -14,12 +14,19 @@ var budgetController = (function(){
 //console.log(budgetController.publicTest(2));
 
 var UIController = (function(){
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    };
+    
     return {
         getInput: function() {
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
         }
     };
