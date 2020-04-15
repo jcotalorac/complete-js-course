edfa48528d450191c679892891a974d704c4a839
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-14 23:07:11 -0500

    Invoking add item to UI from app controller

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -73,7 +73,7 @@ var UIController = (function(){
             
             if(type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-&id&"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMstrings.expenseContainer;
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
@@ -112,6 +112,8 @@ var controller = (function(budgetCtrl, UICtrl){
         input = UICtrl.getInput();
         
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        
+        UICtrl.addListItem(newItem, input.type);
     };
     
     return {
