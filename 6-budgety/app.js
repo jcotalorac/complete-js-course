30b8167bd61674d0d33a482114aa77b918ee2d53
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-14 22:41:06 -0500

    Replacing placeholders with data

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -67,13 +67,15 @@ var UIController = (function(){
             };
         },
         addListItem: function(item, type) {
-            var html;
+            var html, newHtml;
             
             if(type === 'inc') {
                 html = '<div class="item clearfix" id="income-&id&"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
+            
+            newHtml = html.replace('%id%', item.id).replace('%description%', item.description).replace('%value%', item.value);
         },
         getDOMstrings: function() {
             return DOMstrings;
