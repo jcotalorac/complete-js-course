babd907803c2335ccc631e470a47acfff5ae9157
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-16 18:43:24 -0500

    Formatting number values with comma separator, sign and decimal separator

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -209,9 +209,21 @@ var UIController = (function(){
             });
         },
         formatNumber(number, type) {
+            var numSplit, int, dec;
             
             number = Math.abs(number);
             number = number.toFixed(2);
+            
+            numSplit = number.split('.');
+            
+            int = numSplit[0];
+            if(int.length > 3) {
+                int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+            }
+            
+            dec = numSplit[1];
+            
+            return (type === 'exp' ?  '-' : '+') + int + '.' + dec;
         },
         getDOMstrings: function() {
             return DOMstrings;
