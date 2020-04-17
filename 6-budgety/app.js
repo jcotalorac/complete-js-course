4a1ee349b9ccda1c2f2e8477d1b97bd4f9f09572
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-16 19:05:49 -0500

    Completing values formatting

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -196,9 +196,9 @@ var UIController = (function(){
         },
         displayBudget: function(obj){
             
-            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget);
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc);
+            document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp);
             
             if(obj.percentage > 0) {
                 document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
