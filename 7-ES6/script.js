613070362916032c11fa2f514b8bd4cc603956cc
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-17 10:59:50 -0500

    Function scope in ES5

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -1,5 +1,5 @@
 // Let and const
-
+/*
 var name5 = 'Jane Smith';
 var age = 23;
 name5 = 'Jane Miller';
@@ -9,3 +9,16 @@ const name6 = 'Jane Smith';
 let age6 = 23;
 name6 = 'Jane Miller';
 console.log(name6);
+*/
+
+function driverLicence(passedTest){
+    
+    if(passedTest) {
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+        
+        console.log(firstName + ', born in ' + yearOfBirth + ' is now officially allowed to drive a car.');
+    }
+}
+
+driverLicence(true);
