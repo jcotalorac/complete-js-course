95571e09071e94e6859d976d34346b805beb02b7
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-17 15:30:50 -0500

    Workaround in ES5 to treat this inside a callback anonymous function

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -109,8 +109,9 @@ var box5 = {
     color: 'green',
     position: 1,
     clickMe: function() {
+        var self = this;
         document.querySelector('.green').addEventListener('click', function(){
-            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
             alert(str);
         });
     }
