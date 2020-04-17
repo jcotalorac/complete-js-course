b9a179ef5116c3231570273b7d631f8fdaa8279c
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-16 19:35:59 -0500

    Enhancing UI value to show month

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -228,12 +228,14 @@ var UIController = (function(){
             });
         },
         displayMonth: function() {
-            var now, year;
+            var now, months, month, year;
             
             now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
             year = now.getFullYear();
             
-            document.querySelector(DOMstrings.dateLabel).textContent = year;
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
         },
         getDOMstrings: function() {
             return DOMstrings;
