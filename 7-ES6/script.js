1df7b281b600d88106f6d168221193602f1a2b5f
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-18 17:58:00 -0500

    Function in charge of return the average, by mean of the use of reduce array function, that takes all elements and applies an accumulated operation (sum in this case) with and initial value

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -501,6 +501,10 @@ class Street extends Build{
 
 const parks = [new Park('Green Park', 1987, 0.2, 215), new Park('National Park', 1894, 2.9, 3541), new Park('Oak Park', 1953, 0.4, 949)];
 const streets = [new Street('Ocean Avenue', 1999, 1.1, 4), new Street('Evergreen Street', 2008, 2.7, 2), new Street('4th Street', 2015, 0.8), new Street('Sunset Boulevard', 1982, 2.5, 5)];
+
+function average(values){
+    const sum = values.reduce((previousCalculateValue, value, index) => previousCalculateValue + value, 0);
+}
 let parksAverageAge = 3;
 
 console.log('----PARKS REPORT-----');
