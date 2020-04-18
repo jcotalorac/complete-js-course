5129a3dd72fccce903e602cf5f03136969656dfc
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-18 01:02:18 -0500

    Classes in ES6

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -377,3 +377,19 @@ Person5.prototype.calculateAge = function() {
 
 var john5 = new Person5('John', 1990, 'teacher');
 john5.calculateAge();
+
+class Person6 {
+    constructor(name, yearOfBirth, job){
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+const john6 = new Person6('John', 1990, 'teacher');
+john6.calculateAge();
