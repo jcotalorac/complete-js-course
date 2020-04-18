a7464a6c32cd563840465027a6e823e56f6e59ea
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-17 21:58:16 -0500

    Using apply function to assign respectively elements of array to parameters in ES5

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -250,3 +250,7 @@ function addFourAges(a, b, c, d) {
 
 var sum1 = addFourAges(18, 30, 12, 21);
 console.log(sum1);
+
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
