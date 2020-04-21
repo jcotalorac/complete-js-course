8f4c73b5772a2e8f4911fc591ee2d7467ed22eca
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 12:17:09 -0500

    With an example query, save in state the new search object

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -3,7 +3,11 @@ import Search from './models/Search';
 const state = {};
 
 const controlSearch = () => {
-    
+    const query = 'pizza';
+
+    if (query) {
+        state.search = new Search(query);
+    }
 }
 
 document.querySelector('.search').addEventListener('submit', event => {
