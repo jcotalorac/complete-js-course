7e00cfac15844b953ffccfc75ab784361b33ac32
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 15:41:43 -0500

    Including new document element for search results

--- 9-forkify/src/js/views/base.js
+++ 9-forkify/src/js/views/base.js
@@ -1,4 +1,5 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field')
+    searchInput: document.querySelector('.search__field'),
+    resultsOutput : document.querySelector('.results__list')
 };
