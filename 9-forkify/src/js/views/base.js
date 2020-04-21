cef3e77a1cf94e4848572e68f831499b2fef87a1
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 15:53:58 -0500

    Completing information to show in view results

--- 9-forkify/src/js/views/base.js
+++ 9-forkify/src/js/views/base.js
@@ -1,5 +1,5 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
-    resultsOutput : document.querySelector('.results__list')
+    searchResList: document.querySelector('.results__list')
 };
--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -8,16 +8,16 @@ const renderRecipe = recipe => {
             <li>
             <a class="results__link results__link--active" href="#${recipe.recipe_id}">
                 <figure class="results__fig">
-                    <img src="img/test-1.jpg" alt="Test">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">Pasta with Tomato ...</h4>
-                    <p class="results__author">The Pioneer Woman</p>
+                    <h4 class="results__name">${recipe.title}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `;
-    elements.resultsOutput.insertAdjacentHTML('afterbegin', markup)
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
 }
 
 export const renderResults = recipes => {
