a193f0475ea22a33853f16d388db7e5397f2f4fa
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 12:14:16 -0500

    Separating code in function

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -2,8 +2,13 @@ import Search from './models/Search';
 
 const state = {};
 
+const controlSearch = () => {
+    
+}
+
 document.querySelector('.search').addEventListener('submit', event => {
     event.preventDefault();
+    controlSearch();
 });
 
 const search = new Search('pasta');
