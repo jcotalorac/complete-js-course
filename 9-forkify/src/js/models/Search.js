fe1db5cd094e8fd42bfa6560eca47515d3299de8
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 11:42:34 -0500

    Referencing attributes of this object

--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -4,11 +4,11 @@ export default class Search {
     constructor(query) {
         this.query = query;
     }
-    async getResults(query) {
+    async getResults() {
         try {
-            const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-            const recipes = result.data.recipes;
-            console.log(recipes);
+            const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+            this.result = result.data.recipes;
+            console.log(this.result);
         } catch (error) {
             alert(error);
         }
