cf34738d217680d5ce95e7b460a7f74bece83a33
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 11:11:03 -0500

    Showing the error handling to user

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 async function getResults(query) {
     try {
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?query=${query}`);
         console.log(result);
     } catch (error) {
-        alert.log(error);
+        alert(error);
     }
 }
 getResults('Pizza');
