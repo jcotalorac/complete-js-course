eeff2bebaf377bd9af8a9d83e99707451e2748f7
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 11:40:26 -0500

    New asyc method to get the recipes from an API search call

--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -1,5 +1,16 @@
+import axios from 'axios';
+
 export default class Search {
     constructor(query) {
         this.query = query;
+    }
+    async getResults(query) {
+        try {
+            const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+            const recipes = result.data.recipes;
+            console.log(recipes);
+        } catch (error) {
+            alert(error);
+        }
     }
 }
