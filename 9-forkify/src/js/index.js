b6b6c6358af9eac110ba434f4d53a608ddd99509
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 11:15:18 -0500

    Another example to call API

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 async function getResults(query) {
     try {
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?query=${query}`);
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
         console.log(result);
     } catch (error) {
         alert(error);
     }
 }
-getResults('Pizza');
+getResults('pasta');
