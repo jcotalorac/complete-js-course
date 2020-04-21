9216926d43086037dcffd09a77a410866fcd66d7
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 11:09:38 -0500

    Handling errors in ajax call

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 async function getResults(query) {
-    const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-    console.log(result);
+    try {
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        console.log(result);
+    } catch (error) {
+        alert.log(error);
+    }
 }
 getResults('Pizza');
