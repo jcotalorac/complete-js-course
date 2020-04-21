9408550a842aa54774579318eb3d83f15edb8dea
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 10:56:59 -0500

    Importing installed node library axios. This library is most compatible with old browsers than fetch

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 async function getResults(query) {
     const result = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
     console.log(result);
