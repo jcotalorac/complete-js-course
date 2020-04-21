c95dc2744a65c622d9b1677b7c2217d4ef6b1fc7
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 12:11:39 -0500

    Adding event listener for submit button of search

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -2,6 +2,10 @@ import Search from './models/Search';
 
 const state = {};
 
+document.querySelector('.search').addEventListener('submit', event => {
+    
+});
+
 const search = new Search('pasta');
 console.log(search);
 search.getResults();
