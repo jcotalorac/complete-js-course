07e4de3713bfa5a191a473cb87849460f65b73dd
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-21 00:57:41 -0500

    Importing and using named exports

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,3 +1,5 @@
 import str from './models/Search';
 
-import { add, multiply } from './views/searchView';
+import { add, multiply, ID } from './views/searchView';
+
+console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3, 5)}. ${str}`);
