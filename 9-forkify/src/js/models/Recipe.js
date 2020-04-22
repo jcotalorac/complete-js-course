97bcfe17e6dec856912a7e2100a5be30becf9d60
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-22 11:51:01 -0500

    Method to get recipe details

--- 9-forkify/src/js/models/Recipe.js
+++ 9-forkify/src/js/models/Recipe.js
@@ -4,4 +4,13 @@ export default class Recipe {
     constructor(id) {
         this.id = id;
     }
+
+    async getRecipe() {
+        try {
+            const result = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            console.log(result);
+        } catch (error) {
+            alert(error);
+        }
+    }
 }
