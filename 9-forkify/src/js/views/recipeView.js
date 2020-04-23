a14ec729f8db36c3fd692acc891ad3f1aeb0a477
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-22 21:51:54 -0500

    Replacing templates with recipe values

--- 9-forkify/src/js/views/recipeView.js
+++ 9-forkify/src/js/views/recipeView.js
@@ -1,25 +1,25 @@
 export const renderRecipe = recipe => {
     const markup = `
-            <!--
             <figure class="recipe__fig">
-                <img src="img/test-1.jpg" alt="Tomato" class="recipe__img">
+                <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
                 <h1 class="recipe__title">
-                    <span>Pasta with tomato cream sauce</span>
+                    <span>${recipe.title}</span>
                 </h1>
             </figure>
+
             <div class="recipe__details">
                 <div class="recipe__info">
                     <svg class="recipe__info-icon">
                         <use href="img/icons.svg#icon-stopwatch"></use>
                     </svg>
-                    <span class="recipe__info-data recipe__info-data--minutes">45</span>
+                    <span class="recipe__info-data recipe__info-data--minutes">${recipe.time}</span>
                     <span class="recipe__info-text"> minutes</span>
                 </div>
                 <div class="recipe__info">
                     <svg class="recipe__info-icon">
                         <use href="img/icons.svg#icon-man"></use>
                     </svg>
-                    <span class="recipe__info-data recipe__info-data--people">4</span>
+                    <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
                     <span class="recipe__info-text"> servings</span>
                     <div class="recipe__info-buttons">
                         <button class="btn-tiny">
@@ -114,16 +114,14 @@ export const renderRecipe = recipe => {
                 <h2 class="heading-2">How to cook it</h2>
                 <p class="recipe__directions-text">
                     This recipe was carefully designed and tested by
-                    <span class="recipe__by">The Pioneer Woman</span>. Please check out directions at their website.
+                    <span class="recipe__by">${recipe.author}</span>. Please check out directions at their website.
                 </p>
-                <a class="btn-small recipe__btn" href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/" target="_blank">
+                <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
                     <span>Directions</span>
                     <svg class="search__icon">
                         <use href="img/icons.svg#icon-triangle-right"></use>
                     </svg>
                 </a>
             </div>
-            -->
-
     `;
 };
