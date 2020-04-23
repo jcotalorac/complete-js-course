a1bae0183a3917f89e6be44401256df06265d522
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-23 11:11:36 -0500

    Cleaning all selections of recipes before current selection

--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -11,6 +11,11 @@ export const clearResults = () => {
 }
 
 export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(element => {
+        element.classList.remove('results__link--active');
+    });
+
     document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
 };
 
