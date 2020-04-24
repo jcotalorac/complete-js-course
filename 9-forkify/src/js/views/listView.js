5d36b9f60ea7b81077b0615a696bd2674856d738
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-24 17:50:03 -0500

    Deleting shopping list item from view, using html 5 data attribute

--- 9-forkify/src/js/views/listView.js
+++ 9-forkify/src/js/views/listView.js
@@ -2,7 +2,7 @@ import { elements } from './base';
 
 export const renderItem = item => {
     const markup = `
-            <li class="shopping__item">
+            <li class="shopping__item" data-itemid="${item.id}">
                 <div class="shopping__count">
                     <input type="number" value="${item.count}" step="100">
                     <p>${item.unit}</p>
@@ -16,4 +16,9 @@ export const renderItem = item => {
             </li>
     `;
     elements.shopping.insertAdjacentHTML('beforeend', markup);
+};
+
+export const deleteItem = id => {
+    const item = document.querySelector(`[data-itemid="${id}"]`);
+    item.parentElement.removeChild(item);
 };
