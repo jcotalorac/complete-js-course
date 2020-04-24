22d2943261fc4068912c8d91ea075878cb076c92
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-23 21:58:21 -0500

    Matching event to update recipe servings on increase

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -75,5 +75,7 @@ const controlRecipe = async () => {
 elements.recipe.addEventListener('click', event => {
     if(event.target.matches('.btn-decrease', '.btn-decrease *')) {
         state.recipe.updateServings('dec');
+    } else if(event.target.matches('.btn-increase', '.btn-increase *')){
+        state.recipe.updateServings('inc');
     }
 });
