95cd1dccde545c60084ab2226453091d85ef3b03
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-23 23:16:36 -0500

    All ingredients element of view are selected and updated to corresponding ingredients array value

--- 9-forkify/src/js/views/recipeView.js
+++ 9-forkify/src/js/views/recipeView.js
@@ -170,4 +170,9 @@ export const renderRecipe = recipe => {
 
 export const updateServingsIngredients = recipe => {
     document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+    
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((element, i) => {
+        element.textContent = formatCount(recipe.ingredients[i].count);
+    });
 };
