48d4e4417c3713e476212a61f99ebf5a713406b2
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-24 17:38:46 -0500

    Markup and html insertion with filled fields

--- 9-forkify/src/js/views/listView.js
+++ 9-forkify/src/js/views/listView.js
@@ -1,5 +1,19 @@
 import { elements } from './base';
 
 export const renderItem = item => {
-    
+    const markup = `
+            <li class="shopping__item">
+                <div class="shopping__count">
+                    <input type="number" value="${item.count}" step="100">
+                    <p>${item.unit}</p>
+                </div>
+                <p class="shopping__description">${item.ingredient}</p>
+                <button class="shopping__delete btn-tiny">
+                    <svg>
+                        <use href="img/icons.svg#icon-circle-with-cross"></use>
+                    </svg>
+                </button>
+            </li>
+    `;
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
