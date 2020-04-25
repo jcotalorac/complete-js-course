3c5c9df4db630bac268416494e3d2d4b41e2e77a
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-24 20:06:13 -0500

    Add likes method

--- 9-forkify/src/js/models/Likes.js
+++ 9-forkify/src/js/models/Likes.js
@@ -2,4 +2,15 @@ export default class Likes {
     constructor() {
         this.likes = [];
     }
+
+    addLikes(id, title, count, img) {
+        const like = {
+            id,
+            title,
+            count,
+            img
+        }
+        this.likes.push(like);
+        return like;
+    }
 }
