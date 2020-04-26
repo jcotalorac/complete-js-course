9a6bab4a3420939c123ba073d3def8ece63fae87
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-25 19:58:42 -0500

    Discard unsaved ids in data

--- 99-bonus-1/index.js
+++ 99-bonus-1/index.js
@@ -15,7 +15,7 @@ const server = http.createServer((request, response) => {
     if(pathName === '/products' || pathName === '/') {
         response.writeHead(200, {'Content-type': 'text/html'});
         response.end('This is the products page!');
-    } else if(pathName === '/laptop') {
+    } else if(pathName === '/laptop' && id < laptopData.lenght) {
         response.writeHead(200, {'Content-type': 'text/html'});
         response.end(`This is the laptop page for laptop ${id}!`);
     } else {
