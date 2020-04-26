65edf953189e874a8861064bda9837051573ea6c
Juan Carlos Otálora <jcotalorac@gmail.com>
2020-04-25 21:07:26 -0500

    Correcting file path

--- 99-bonus-1/index.js
+++ 99-bonus-1/index.js
@@ -17,7 +17,7 @@ const server = http.createServer((request, response) => {
         response.end('This is the products page!');
     } else if(pathName === '/laptop' && id < laptopData.length) {
         response.writeHead(200, {'Content-type': 'text/html'});
-        fs.readFile(`${__dirname}`/templates/template-laptopData.html, 'utf-8', (err, data) => {
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
             const laptop = laptopData[id];
             let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName).replace(/{%IMAGE%}/g, laptop.image).replace(/{%PRICE%}/g, laptop.price).replace(/{%SCREEN%}/g, laptop.screen).replace(/{%CPU%}/g, laptop.cpu).replace(/{%STORAGE%}/g, laptop.storage).replace(/{%RAM%}/g, laptop.ram).replace(/{%DESCRIPTION%}/g, laptop.description);
             response.end(output);
